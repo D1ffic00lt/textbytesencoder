3528e960f19ebe165cb89a7ccccdd11b31bfb63b
D1ffic00lt <dm.filinov@gmail.com>
2022-11-09 18:11:19 +0700

    first commit

---
+++ textbytesencoder/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python Encoder
+~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Discord API.
+
+:copyright: (c) 2022-present D1ffic00lt
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from textbytesencoder.textbytesencoder import *
+
+__version__ = "0.1.4"
+__title__ = 'Encoder'
+__author__ = "D1ffic00lt"
+__copyright__ = "Copyright 2013-2022 {}".format(__author__)
